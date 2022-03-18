4d4caae4df390e264a760dc0463ecb66db2eaa6f
Murugi <lizar.murugi@gmail.com>
2022-03-18 06:13:51 -0700

    Fixing error in 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * prints 10 times the numbers, from 0 to 14
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by a new line
  */
 void more_numbers(void)
